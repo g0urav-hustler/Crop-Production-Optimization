918ec452c6737ddb3dacae68bb32d99d8eaec5c7
g0urav_hustler <gouravchouhan966@gmail.com>
2021-12-18 00:13:43 +0530

    rearrange data

---
+++ README.md
@@ -0,0 +1,4 @@
+# Crop-Production-Optimization
+Website - https://crop-production-optimize.herokuapp.com/
+
+This is site which predict the best crops to be grown on the field .
--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template
 import pickle 
 import os
-
 app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
 
@@ -29,4 +28,4 @@ def predict():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host = '0.0.0.0', port = port)
+    app.run(debug = False, port = port)
